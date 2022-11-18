304e6ce1fe0f0adc4d1b3bc6595127af62ffc86f
Hirendra-Gurnani <92821585+Hirendra-Gurnani@users.noreply.github.com>
2022-11-19 02:26:18 +0530

    Major Frontend Revamp

--- foodtracker/foodtracker/static/js/profile.js
+++ foodtracker/foodtracker/static/js/profile.js
@@ -2,5 +2,5 @@ function depression() {
     var w = document.getElementById("weight").innerHTML
     var h = document.getElementById("height").innerHTML
     var res = w/(h*h)
-    document.getElementById("res").innerHTML="&emsp; &emsp; &emsp; &emsp; &emsp; &nbsp; BMI- "+res.toFixed(2)+" kg/m<sup>2</sup>"
+    document.getElementById("res").innerHTML=res.toFixed(2)+" kg/m<sup>2</sup>"
 }
